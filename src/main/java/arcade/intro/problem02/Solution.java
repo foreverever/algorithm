90b8bdf84b5d973726803d2a44fd1574bf12187c
TaeSu <lollolzkk@gmail.com>
2019-08-20 17:54:00 +0900

    Arcade Intro_problem02 complete

---
+++ src/main/java/arcade/intro/problem02/Solution.java
@@ -0,0 +1,11 @@
+package arcade.intro.problem02;
+
+public class Solution {
+    int centuryFromYear(int year) {
+        int division = 100;
+        int century = year / division;
+
+        if (year % division != 0) century++;
+        return century;
+    }
+}
