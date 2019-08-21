3d9fe1d73de58aee26a1f0baa1ff4f862007724b
TaeSu <lollolzkk@gmail.com>
2019-08-21 18:04:00 +0900

    Arcade Intro_problem03 complete

---
+++ src/main/java/arcade/intro/problem03/Solution.java
@@ -0,0 +1,17 @@
+package arcade.intro.problem03;
+
+public class Solution {
+    boolean checkPalindrome(String inputString) {
+        int s = 0, e = inputString.length() - 1;
+        while (s < e) {
+            if (inputString.charAt(s) != inputString.charAt(e)) return false;
+            s++;
+            e--;
+        }
+        return true;
+    }
+
+    boolean checkPalindrome2(String inputString) {
+        return inputString.equals(new StringBuffer(inputString).reverse().toString());
+    }
+}
